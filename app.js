22cb26b3fedea308f0e02676b5bfbe39732ab39e
VirTorrazza <torrazzavirginia@gmail.com>
2024-04-07 18:09:37 -0300

    changes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import cartRouter from './src/routers/carts.routers.js';
 import viewsRouter from './src/routers/views.routers.js';
 import {Server} from 'socket.io';
 import handlebars from 'express-handlebars';
-import cors from 'cors'
+import cors from 'cors';
+import ProductManager from './src/data/productManager.js';
 
 const app = express();
 app.use(express.static('src/public'));
@@ -20,3 +21,13 @@ app.use(cors()); // enable CORS for all routes
 
 const httpServer= app.listen(8080,()=>console.log("Server Up")); 
 const socketServer= new Server (httpServer);
+const productManager= new ProductManager('./src/data/products.json');
+
+socketServer.on('connection', async (clientSocket) => {
+    console.log ("Client connected");
+    clientSocket.on('productsList', async data => {
+        let newData= await productManager.getProducts();
+        console.log("El cliente envia "+ JSON.stringify(data))
+        socketServer.emit('updateProducts', newData);
+    });
+});
--- src/public/index.js
+++ src/public/index.js
@@ -17,7 +17,7 @@ function addProductToTable(event) {
   };
 
   fetch('/products', {
-    method: 'post',
+    method: 'POST',
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json'
@@ -45,30 +45,90 @@ function addProductToTable(event) {
   });
 }
 
-function deleteProduct(id) {
-  Swal.fire({
-    title: "Product deletion",
-    text: "Are you sure you want to delete this product?",
-    icon: "warning",
-    width: '300px',
-    height: '110px',
-    allowOutsideClick: false,
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes"
-  }).then((result) => {
-    if (result.isConfirmed) {
+async function deleteProduct(pid) {
+  try {
+    const response = await fetch(`/products/${pid}`, {
+      method: 'DELETE'
+    });
+    
+    if (response.ok) {
+      const result = await response.json();
+      
       Swal.fire({
-        title: "Deleted!",
-        text: "The product has been deleted.",
-        icon: "success",
+        title: "Product deletion",
+        text: "Are you sure you want to delete this product?",
+        icon: "warning",
         width: '300px',
         height: '110px',
-        allowOutsideClick: false
+        allowOutsideClick: false,
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          socket.emit('productsList', result.payload);
+          Swal.fire({
+            title: "Deleted!",
+            text: "The product has been deleted.",
+            icon: "success",
+            width: '300px',
+            height: '110px',
+            allowOutsideClick: false
+          });
+        }
       });
+    } else {
+      const errorMessage = await response.text();
+      throw new Error(errorMessage || 'Failed to delete product');
     }
-  }); 
+  } catch (error) {
+    console.error('Error deleting product:', error.message);
+    
+  }
 }
+
+socket.on("updateProducts",(data) =>{
+  let table= document.getElementById("real-time-products-table");
+  let HTMLTable=`
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Code</th>
+      <th scope="col">Price</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Category</th>
+    </tr>
+  </thead>`
+
+  data.forEach(element => {
+
+    HTMLTable += `
+    <tr>
+          <td>${element.title}</td>
+          <td>${element.description}</td>
+          <td>${element.code}</td>
+          <td>${element.price}</td>
+          <td>${element.stock}</td> 
+          <td>${element.category}</td>
+          <td>
+            <div class="btn-group" role="group" aria-label="Product Actions">
+              <button class="btn btn-outline-danger" id="delete-product-button" data-product-id="delete-${element.id}" type="button" data-bs-toggle="tooltip" onclick="deleteProduct(${this.id})" data-bs-placement="top" title="Delete">
+                <i class="fas fa-trash"></i>
+              </button>
+            </td>
+            <td>
+              <button class="btn btn-outline-primary" id="add-product-button" data-product-id="add-${element.id}" type="button" data-bs-toggle="tooltip" onclick="addProduct()" data-bs-placement="top" title="Add">
+                <i class="fas fa-plus"></i>
+              </button>
+            </td>
+          </div>
+        </tr>
+    `
+  });
+
+table.innerHTML=HTMLTable;
+})
 
 document.getElementById("newProductForm").addEventListener("submit", addProductToTable);
