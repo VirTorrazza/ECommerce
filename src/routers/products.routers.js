ea93d3bf6cdfaef55cfd868971d8be3a220162f9
VirTorrazza <torrazzavirginia@gmail.com>
2024-05-07 14:47:27 -0300

    delete method

--- src/routers/products.routers.js
+++ src/routers/products.routers.js
@@ -1,10 +1,7 @@
 import { Router } from "express";
-import ProductManager from "../data/productManager.js";
-import productModel from "../models/products.model.js";
+import productModel from "../models/products.model.js"
 
 const productRouter = Router();
-const productManager= new ProductManager('./src/data/products.json');
-
 
 productRouter.get('/', async (req, res)=>{
     let status;
@@ -116,32 +113,42 @@ productRouter.post('/', async (req,res)=>{
 }
 })
 
-productRouter.put('/:pid', async (req,res)=>{
-    try{
-        let product= req.body;
-        let id= parseInt(req.params.pid);
-        let newProduct= await productManager.updateProduct(id,product);
-        if (typeof newProduct === 'string' && newProduct.startsWith('Error')) {
-            return res.status(400).json({ error: "Product does not exist" });
-        } else {
-            return res.status(201).json({ message: 'Product updated successfully', payload: newProduct });
-        }
-    }catch (error){
-        return res.status(500).json({ error: 'Internal Server Error' })
-}
-}
-)
+productRouter.put('/:pid', async (req, res) => {
+    try {
+
+        let updateFields = {};
+        if (req.body.title) updateFields.title = req.body.title;
+        if (req.body.description) updateFields.description = req.body.description;
+        if (req.body.code) updateFields.code = req.body.code;
+        if (req.body.price) updateFields.price = req.body.price;
+        if (req.body.stock) updateFields.stock = req.body.stock;
+        if (req.body.category) updateFields.category = req.body.category;
+        if (req.body.thumbnails) updateFields.thumbnails = req.body.thumbnails;
+
+        let pid = req.params.pid;
+        let newProduct = await productModel.findByIdAndUpdate(pid, updateFields, { returnDocument: 'after' });
+
+        return res.status(200).json({ payload: newProduct });
+        
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 productRouter.delete('/:pid', async (req,res)=>{
-    let pid= parseInt(req.params.pid);
-    let newProducts=await productManager.deleteProduct(pid);
     try{
-        if (typeof newProducts === 'string' && newProducts.startsWith('Error')) {
-            return res.status(400).json({ error: "The product cannot be deleted because it does not exist" });
-        } else {
-            return res.status(204).end();
+        let pid= req.params.pid;
+        try {
+            let product=await productModel.findById(pid);
+            if(!product) return res.status(404).json ({error: "404: Product not found"})
+
+        } catch(error){
+            return res.status(404).json ({error: "404: Parser Error.Cast Error"+ error});
         }
-    }catch (error){
+        await productModel.findByIdAndDelete(pid);
+        return res.status(200).json({ message: "Product deleted successfully" });
+
+    } catch (error){
         return res.status(500).json({ error: 'Internal Server Error' });
 }
 
