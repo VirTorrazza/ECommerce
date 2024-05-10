69903c889ac3f1c219ec6acb5900c1727cffaae6
VirTorrazza <torrazzavirginia@gmail.com>
2024-05-10 16:18:36 -0300

    minor changes

--- src/routers/session.views.router.js
+++ src/routers/session.views.router.js
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import { privateRoutes,publicRoutes } from '../middlewares/auth.middleware.js';
+import userModel from '../models/user.model.js';
 
 const sessionViewsRouter= Router();
 
@@ -10,5 +11,23 @@ sessionViewsRouter.get('/register',(req,res)=>{
 sessionViewsRouter.get('/', (req,res)=>{
     res.render('sessions/login');
 })
+sessionViewsRouter.get('/profile', async (req, res) => {
+    try {
+        const newUser = await userModel.create({
+            first_name: "John",
+            last_name: "Doe",
+            email: "john.doe@example.com",
+            age: 30,
+            password: "password123",
+            role: "user"
+        });
+
+        res.render('sessions/profile', newUser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).send("Error creating user");
+    }
+});
+
 
 export default sessionViewsRouter 
