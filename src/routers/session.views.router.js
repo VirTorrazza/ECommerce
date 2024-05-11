f1c08db6b15f6c6c9fe8cbb94a6c74382772e704
VirTorrazza <torrazzavirginia@gmail.com>
2024-05-11 15:53:58 -0300

    functionality changes

--- src/routers/session.views.router.js
+++ src/routers/session.views.router.js
@@ -12,20 +12,7 @@ sessionViewsRouter.get('/', (req,res)=>{
     res.render('sessions/login');
 })
 sessionViewsRouter.get('/profile', (req, res) => {
-    try {
-        const newUser = {
-            first_name: "John",
-            last_name: "Doe",
-            email: "john.doe@example.com",
-            age: 30,
-            role: "user"
-        };
-
-        res.render('sessions/profile', {user: newUser});
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).send("Error creating user");
-    }
+    res.render('sessions/profile', req.session.user );
 });
 
 
