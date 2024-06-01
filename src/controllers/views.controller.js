351eeb5a8f427d5769ffeca3c548f988eef66bca
VirTorrazza <torrazzavirginia@gmail.com>
2024-06-01 14:55:01 -0300

    adding views controller

---
+++ src/controllers/views.controller.js
@@ -0,0 +1,71 @@
+import productModel from "../models/products.model.js";
+import config from "../config/config.js";
+
+const PORT =config.apiserver.port;
+
+export async function getRealTimeProducts (req, res){
+    const products = await productModel.find().lean().exec();
+    res.render('realtimeproducts', {products}); // view name
+}
+
+export async function getHomePage (req, res){
+    let status;
+    const limit = req.query.limit || 10;
+    const page = req.query.page || 1;
+    const filters = {};
+    if(req.query.category)  filters.category = req.query.category;
+    if(req.query.stock)     filters.stock = req.query.stock;
+    const paginateOptions = { lean:true, limit, page };
+    if(req.query.sort === 'asc') paginateOptions.sort = {price : 1};
+    if(req.query.sort === 'desc') paginateOptions.sort = {price : -1};
+    let productsPaginated= await productModel.paginate({}, paginateOptions);
+    if (productsPaginated){
+        status= "success";
+    }
+    else{
+        status="error"
+    }
+
+    let prevLink;
+    if(!req.query.page){
+        prevLink = `http://${req.hostname}:${PORT}${req.originalUrl}&page=${productsPaginated.prevPage}`;
+    }else{
+        let urlMod = req.originalUrl.replace(`page=${req.query.page}`,`page=${productsPaginated.prevPage}`);
+        prevLink = `http://${req.hostname}:8080${urlMod}`;
+    }
+
+    let nextLink;
+    if(!req.query.page){
+        nextLink = `http://${req.hostname}:8080${req.originalUrl}&page=${productsPaginated.nextPage}`;
+    }else{
+        let urlMod = req.originalUrl.replace(`page=${req.query.page}`,`page=${productsPaginated.nextPage}`);
+        nextLink = `http://${req.hostname}:8080${urlMod}`;
+    }
+
+    let totalPages = productsPaginated.totalPages;
+    let currentPage = productsPaginated.page;
+    let pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pages.push({
+            page: i,
+            link: `http://localhost:8080?page=${i}`,
+            isActive: i === currentPage
+        });
+    }
+
+    const user= req.user.user;
+    res.render('home', {
+        status,
+        user,
+        products: productsPaginated.docs,
+        paginateInfo: {
+            totalPages,
+            page: currentPage,
+            hasPrevPage: productsPaginated.hasPrevPage,
+            hasNextPage: productsPaginated.hasNextPage,
+            prevLink,
+            nextLink
+        },
+        pages
+    });
+}
