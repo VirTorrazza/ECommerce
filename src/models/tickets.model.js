e0adbf574c568d4df452cc96d2eefe64d13cadb6
VirTorrazza <torrazzavirginia@gmail.com>
2024-06-04 21:41:02 -0300

    feat: adding tickets model

---
+++ src/models/tickets.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const ticketsCollection = 'tickets'; 
+const ticketSchema = new mongoose.Schema({
+	code: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    products: {
+        type: [{
+            _id: false,
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "products"
+            },
+            price: Number,
+            quantity: Number
+        }]
+    },
+    purchaser:{ type: String, ref: "users" },
+    amount: { type: Number }, 
+}, 
+{
+    timestamps: {
+        createdAt: 'purchase_datetime'
+    }
+});
+
+export default { ticketsCollection, ticketSchema };
