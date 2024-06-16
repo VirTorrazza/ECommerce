f109493541c8fc34fa767c85c9a77c41b5927d16
VirTorrazza <torrazzavirginia@gmail.com>
2024-06-16 10:46:00 -0300

    feat: adding persistence type

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
+    persistence:{
+        type: process.env.PERSISTENCE
+    },
     apiserver: {
         port: process.env.PORT
     },
